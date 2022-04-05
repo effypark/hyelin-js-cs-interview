a0d5d9577df6ce16fca8c2b59b8d05523e542c7c
박혜린 <likehyelin@gmail.com>
2022-04-05 21:42:30 +0900

    JS - 동기&비동기 차이

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,16 @@
-function hello(name) {
-  let _name = name; // 변수명 앞에 underscore(_)를 포함하면 private val임을 의미
-  return function () {
-    // 클로저 함수
-    console.log('Hello, ' + _name);
-  };
-}
+// function hello(name) {
+//   let _name = name; // 변수명 앞에 underscore(_)를 포함하면 private val임을 의미
+//   return function () {
+//     // 클로저 함수
+//     console.log('Hello, ' + _name);
+//   };
+// }
 
-let hello1 = hello('혜린');
-hello1(); // output = Hello, 혜린
+// let hello1 = hello('혜린');
+// hello1(); // output = Hello, 혜린
+
+console.log('first');
+setTimeout(() => {
+  console.log('second');
+}, 0);
+console.log('third');
